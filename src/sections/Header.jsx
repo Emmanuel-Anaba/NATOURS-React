f32b5988d56f90e0b76eae0eae05c0a3a60ca17d
Emmanuel-Anaba <emmaanaba29@gmail.com>
2024-05-08 04:41:21 +0100

    minor changes

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       <div className="grid gap-8 absolute left-1/2 -translate-x-1/2 top-[40%] -translate-y-[60%]">
         <div className="text-center font-medium text-white">
           <p className="font-bold md:font-medium pb-8 text-2xl md:text-[4rem] ls-md md:ls-lg">OUTDOORS</p>
-          <p className="font-medium text-base ls-sm">IS WHERE LIFE HAPPENS</p>
+          <p className="font-medium text-base ls-xs md:ls-sm">IS WHERE LIFE HAPPENS</p>
         </div>
         <a href="#tours" className="bg-white btn mx-auto">
           DISCOVER OUR TOURS
